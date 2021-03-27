980a43cb823b4f4e70ef973c397f4fe23d87adac
kihBootCamp <kimberleyheuer@hotmail.com>
2021-03-27 07:09:47 -0500

    using react-bootstrap-table

---
+++ src/components/Buttons.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Buttons = () => {
+    return (
+        <button className="btn btn-primary" onClick={this.handleGender}>
+        Gender
+      </button>
+    )
+}
+
+export default Buttons
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import API from "./API.js";
 import Table from "./Table.js";
 import Spinner from "./Spinner.js";
+import BootstrapTable from "react-bootstrap-table-next";
 
 //import Styled from "styled-components"
 
@@ -10,57 +11,79 @@ class Employee extends Component {
         //search: "",
         employeeList: []
     };
-        
+
 
     componentDidMount() {
 
         //
         // could keep the getEmployees code in here
-        
-       this.getEmployees();
+
+        this.getEmployees();
     };
 
     getEmployees = () => {
         API.getEmployees()
-        .then(res => {
-            console.log(res);
-            this.setState({employeeList: res.data.results})
-            console.log(this.state.employeeList);
-        })
-        .catch(err => console.log(err));
+            .then(res => {
+                console.log(res);
+                this.setState({ employeeList: res.data.results })
+                console.log(this.state.employeeList);
+            })
+            .catch(err => console.log(err));
     }
 
-
+    
 
     render() {
 
-        return (
-            <div>
-            {/* < Spinner /> */}
-            <p>hello</p>
-            <table>
-                <tbody>
-                   <tr>
-                      
-                       <td> Name </td>
-                       <td> Cell </td>
-                    </tr>
-                   
-            {this.state.employeeList.map((emp, index) => (
-                <p key={index}>
-                    {emp.name.last}  {emp.name.first}  {emp.cell}
-                </p>
-            )
-              
-            )}
+        const columns = [{
+            dataField: "id.value",
+            text: "Cell"
+          }, {
+            dataField: "name.last",
+            text: "Last Name"
+          }, {
+            dataField: "name.first",
+            text: "First Name"
+          }, {
+            dataField: "dob.age",
+            text: "Age"
+          }, {
+            dataField: "gender",
+            text: "Gender"
+          }];
 
-</tbody>
-                    </table>
-                    
+        return (
+            // <div>
+            //     <table>
+            //         <tbody>
+            //             <tr>
+            //                 <td> Name </td>
+            //                 <td> Cell </td>
+            //                 <td> City </td>
+            //                 <td> Gender </td>
+            //             </tr>
+
+            //             {this.state.employeeList.map((emp, index) => (
+            //                 <tr key={index}>
+            //                     <td> {emp.name.last}  {emp.name.first} </td>
+            //                     <td> {emp.cell} </td>
+            //                     <td> {emp.location.city} </td>
+            //                     <td> {emp.gender} </td>
+            //                 </tr>
+            //             )
+            //             )}
+            //         </tbody>
+            //     </table>
+            // </div>
+
+            <div className="App">
+                <p className="Table-header">Employee Table</p>
+
+                <BootstrapTable keyField="id" data={this.state.employeeList} columns={columns} />
             </div>
 
         );
-}
+    }
 
 }
 
